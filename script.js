07c561971c211f2f6fc53b7adfe1af74e2d9b988
kamuganteng785-dotcom <kamuganteng785@gmail.com>
2026-05-31 12:36:59 +0700

    Project awal web kelulusan

---
+++ script.js
@@ -0,0 +1,23 @@
+body{
+font-family:Arial;
+background:#f2f2f2;
+}
+
+.container{
+width:400px;
+margin:100px auto;
+background:white;
+padding:20px;
+border-radius:10px;
+}
+
+input{
+width:100%;
+padding:10px;
+margin-bottom:10px;
+}
+
+button{
+width:100%;
+padding:10px;
+}
