16c952c338b52812d294301a7ae10eba22cb2bfb
kamuganteng785-dotcom <kamuganteng785@gmail.com>
2026-05-31 15:39:05 +0700

    add files

--- script.js
+++ script.js
@@ -1,23 +1,75 @@
-body{
-font-family:Arial;
-background:#f2f2f2;
+import { createClient } from 'https://esm.sh/@supabase/supabase-js'
+
+// GANTI DENGAN URL PROJECT SUPABASE ANDA
+const supabaseUrl = 'https://xppzbclbhqgeyvlqbvlx.supabase.co'
+
+// GANTI DENGAN PUBLISHABLE KEY SUPABASE ANDA
+const supabaseKey = 'sb_publishable_quDixocgAAC-uaKu4MKZsw_b-dve2c-'
+
+const supabase = createClient(
+  supabaseUrl,
+  supabaseKey
+)
+
+async function cekKelulusan() {
+
+  const nisn = document
+    .getElementById('nisn')
+    .value
+    .trim()
+
+  const hasil = document.getElementById('hasil')
+
+  hasil.innerHTML = 'Mencari data...'
+
+  try {
+
+    const { data, error } = await supabase
+      .from('kelulusan')
+      .select('*')
+
+    console.log('DATA:', data)
+    console.log('ERROR:', error)
+
+    if (error) {
+      hasil.innerHTML = 'ERROR: ' + error.message
+      return
+    }
+
+    const siswa = data.find(
+      item => item.nisn === nisn
+    )
+
+    if (siswa) {
+
+     const warna =
+siswa.status === 'LULUS'
+? '#00ff88'
+: '#ff5c5c'
+
+hasil.innerHTML = `
+<h2>${siswa.nama}</h2>
+
+<h3 style="color:${warna}">
+STATUS : ${siswa.status}
+</h3>
+`
+
+    } else {
+
+      hasil.innerHTML =
+        'NISN tidak ditemukan'
+
+    }
+
+  } catch (err) {
+
+    console.error(err)
+
+    hasil.innerHTML =
+      'Gagal terhubung ke database'
+
+  }
 }
 
-.container{
-width:400px;
-margin:100px auto;
-background:white;
-padding:20px;
-border-radius:10px;
-}
-
-input{
-width:100%;
-padding:10px;
-margin-bottom:10px;
-}
-
-button{
-width:100%;
-padding:10px;
-}
+window.cekKelulusan = cekKelulusan
